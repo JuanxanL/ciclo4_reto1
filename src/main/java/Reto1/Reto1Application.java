857bd0fc9647b883aa2d9ea11304b09a182d4f18
Juan Lincon Sanchez <juan.sanchez2.mt@correo.usa.edu.co>
2021-11-24 22:05:05 -0500

    Agregamos reto 1

---
+++ src/main/java/Reto1/Reto1Application.java
@@ -0,0 +1,15 @@
+package Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
